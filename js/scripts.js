ea7a36cb6d709bc78f3c7be13d4802e2b5594f70
wcjameson <williamjameson90@gmail.com>
2022-01-14 15:21:01 -0800

    Add additional CSS styling

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,10 @@ $(document).ready(function() {
   $("button#submit-form").click(function(event) {
     event.preventDefault();
     let userSelection1 = $("input:radio[name=answer1Radio]:checked").val();
-    console.log(userSelection1);
     let userSelection2 = $("input:radio[name=answer2Radio]:checked").val();
-    console.log(userSelection2);
     let userSelection3 = $("input:radio[name=answer3Radio]:checked").val();
-    console.log(userSelection3);
     let userSelection4 = $("input:radio[name=answer4Radio]:checked").val();
-    console.log(userSelection4);
     let userSelection5 = $("input:radio[name=answer5Radio]:checked").val();
-    console.log(userSelection5);
       
     if(userSelection1 === "option1" && userSelection2 === "option2" && userSelection3 != "option2") {
       $(".answer").hide();
